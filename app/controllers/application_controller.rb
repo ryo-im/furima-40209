658f49682bedc402d48656557e02bdf56c18d7d7
ryo-im <ir.nkym6916@gmail.com>
2024-04-27 05:46:59 +0900

    Basic認証テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :check_env_variables
   before_action :basic_auth
 
   private
+
+
+  def check_env_variables
+    Rails.logger.info 'BASIC_AUTH_USER: ' + ENV["BASIC_AUTH_USER"].to_s
+    Rails.logger.info 'BASIC_AUTH_PASSWORD: ' + ENV["BASIC_AUTH_PASSWORD"].to_s
+   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :last_name, :first_name,
